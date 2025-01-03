2dea207e62b11c26c87331197ed7ef49b8be36c9
newgate2601 <newgate2601@gmail.com>
2025-01-03 12:42:05 +0700

    fix: component page

--- src/components/pages/Messages/index.tsx
+++ src/components/pages/Messages/index.tsx
@@ -8,34 +8,32 @@
   import MessageList from "./MessageList";
 
   const MessagePage = () => {
-    const { t } = useTranslation();
-    const [open, setOpen] = useState(false);
-
     const handleClose = () => {
-      setOpen(false);
+      setOpenn(false);
     };
+
+    const { t } = useTranslation();
+    const [open, setOpenn] = useState(false);
 
     return (
       <div className="flex flex-col">
         <div className="fixed top-0 left-0 w-full bg-white pt-24 shadow-md transition-transform duration-300 lg:h-screen lg:w-96 ">
           <div className="mx-4 flex items-center justify-between">
             <h2 className="text-3xl font-medium">{t("message.chatsection")}</h2>
+            <SearchChat />
 
+            <DrawerCustomize title={t("message.creategroup")} open={open} onClose={handleClose}>
+              <CreateGroupChat onSuccess={handleClose} />
+            </DrawerCustomize>
+
+            <MessageList />
             <button
               className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-200 text-lg font-medium hover:bg-gray-300 "
-              onClick={() => setOpen(true)}
+              onClick={() => setOpenn(true)}
             >
               <IconCustomize name="create" size={25} color="black" />
             </button>
           </div>
-
-          <SearchChat />
-
-          <DrawerCustomize title={t("message.creategroup")} open={open} onClose={handleClose}>
-            <CreateGroupChat onSuccess={handleClose} />
-          </DrawerCustomize>
-
-          <MessageList />
         </div>
 
         <div className="pt-40 lg:pl-96 lg:pt-2">
@@ -44,5 +42,4 @@
       </div>
     );
   };
-
   export default MessagePage;
